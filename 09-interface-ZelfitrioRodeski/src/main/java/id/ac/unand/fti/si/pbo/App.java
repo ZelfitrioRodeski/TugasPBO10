eb08a03cda8963ce8965331619b25b312bc88c3c
ZelfitrioRodeski <144987490+ZelfitrioRodeski@users.noreply.github.com>
2023-11-15 21:49:58 +0700

    Update App.java

--- 09-interface-ZelfitrioRodeski/src/main/java/id/ac/unand/fti/si/pbo/App.java
+++ 09-interface-ZelfitrioRodeski/src/main/java/id/ac/unand/fti/si/pbo/App.java
@@ -9,12 +9,14 @@ public class App {
         Member memberreguler1 = new MemberReguler();
         Member memberplatinum1 = new MemberPlatinum();
         Member membergold1 = new MemberGold();
-   
+
+        //Menghitung total bayar
    int totalbayarmembersilver = membersilver1.hitungTotalBayar(1000000); 
    int totalbayarmemberreguler = memberreguler1.hitungTotalBayar(1000000); 
    int totalbayarmemberplatinum = memberplatinum1.hitungTotalBayar(1000000); 
    int totalbayarmembergold = membergold1.hitungTotalBayar(1000000); 
 
+        //Menampilkan total bayar
    System.out.println ("total ongkir member silver: " + totalbayarmembersilver);
    System.out.println ("total ongkir member reguler: " + totalbayarmemberreguler);
    System.out.println ("total ongkir member platinum: " + totalbayarmemberplatinum);
